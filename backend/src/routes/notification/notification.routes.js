4dd88b406a4ad8c24331f577dcd3f71cba648136
daianabetsabeselis@gmail.com <daianabetsabeselis@gmail.com>
2025-03-26 15:42:32 -0300

    Cambiosde nombre a las funciones en controladores y routes  en el backend

---
+++ backend/src/routes/notification/notification.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const router = express.Router();
+import controller from "../../controllers/notification/notification.controller.js";
+
+router.get("/notifications/", getAllNotification);
+router.get("/notifications/:id", getNotificicationByNotification);
+router.post("/notifications",createNotification ); // Ruta corregida
+router.put("/notifications/:id",updateNotification);
+router.delete("/notifications/:id", deleteNotification);
+
+export default router;
