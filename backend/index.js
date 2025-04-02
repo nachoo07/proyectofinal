bdcc057f31f9042e4d84e268a7ae1eaf27cc08eb
Daiana Selis <daianabetsabeselis@gmail.com>
2025-04-02 01:26:32 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import { PORT } from './src/config/config.js'; // Importa el puerto desde config.js
-
+import motionRoutes from './src/routes/motion.routes.js'; // Importa las rutas de motion
 
 const app = express();
 
@@ -11,6 +11,7 @@ app.use(express.json()); // Para parsear JSON en las solicitudes
 app.use(morgan('dev')); // Logs de solicitudes HTTP
 app.use(cors()); // Habilita CORS para permitir solicitudes cross-origin
 
+app.use('/api/motion', motionRoutes); // Rutas de motion
 
 // Ruta base
 app.get('/', (req, res) => {
