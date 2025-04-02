ec66dbd9a32515bc9ee94eee9878f3242988a361
Daiana Selis <daianabetsabeselis@gmail.com>
2025-04-02 01:31:59 -0300

    Update notification.routes.js

--- backend/src/routes/notification/notification.routes.js
+++ backend/src/routes/notification/notification.routes.js
@@ -1,12 +1,18 @@
 import express from "express";
 
 const router = express.Router();
-import controller from "../../controllers/notification/notification.controller.js";
+import {
+  createNotification,
+  deleteNotification,
+  getAllNotification,
+  getNotificicationByNotification,
+  updateNotification,
+} from "../../controllers/notification/notification.controller.js";
 
 router.get("/notifications/", getAllNotification);
 router.get("/notifications/:id", getNotificicationByNotification);
-router.post("/notifications",createNotification ); // Ruta corregida
-router.put("/notifications/:id",updateNotification);
+router.post("/notifications", createNotification); // Ruta corregida
+router.put("/notifications/:id", updateNotification);
 router.delete("/notifications/:id", deleteNotification);
 
 export default notificationRouter;
