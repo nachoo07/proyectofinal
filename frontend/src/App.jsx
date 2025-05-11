93d6f818c9427ec6c83c777020839eab4628a95b
Daiana Selis <daianabetsabeselis@gmail.com>
2025-05-11 17:16:13 -0300

    Merge dev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,19 @@ import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from './theme';
 import { NotificationProvider } from "./context/notification/notificationContext";
 import Router from './routes/router.jsx';
+import { SharesProvider } from './context/share/ShareContext';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <NotificationProvider>
-      <SharesProvider>
-        <Router/>
-      </SharesProvider>
+        <SharesProvider>
+          <Router />
+          <ToastContainer />
+        </SharesProvider>
       </NotificationProvider>
     </ThemeProvider>
   );
--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -3,6 +3,7 @@ import PageNotification from '../pages/notification/PageNotification';
 import PageMotion from '../pages/motion/PageMotion';
 import NavBar from '../components/NavBar/NavBar';
 import PageReport from '../pages/report/PageReport';
+import SharesPage from './pages/share/PageShare';
 
 function Router() {
   return (
@@ -15,6 +16,8 @@ function Router() {
 
         <Route path="/notifications" element={<PageNotification />} />
         <Route path="/reports" element={<PageReport/>} />
+
+        <Route path="/shares" element={<SharesPage />} />
       </Routes>
     </BrowserRouter>
   );
