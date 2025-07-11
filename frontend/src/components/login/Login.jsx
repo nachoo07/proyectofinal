e8e70d1d3f037d19e05fca6cad05995d574b84d9
Daiana Selis <daianabetsabeselis@gmail.com>
2025-07-11 20:42:36 -0300

    cambios a motion

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import  { useState, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LoginContext } from '../../context/login/LoginContext';
 
